c36199cf05ba400a93e8785426819bd1e055e456
yang <10052036+alexyyhhh@user.noreply.gitee.com>
2025-12-09 16:30:30 +0800

    维持登录态

--- internal/web/middleware/login.go
+++ internal/web/middleware/login.go
@@ -1,15 +1,19 @@
 package middleware
 
 import (
+	"encoding/gob"
+	"fmt"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
 
 type LoginMiddlewareBuilder struct {
 }
 
 func (m *LoginMiddlewareBuilder) CheckLogin() gin.HandlerFunc {
+	gob.Register(time.Now())
 	return func(ctx *gin.Context) {
 		path := ctx.Request.URL.Path
 		if path == "/users/signup" || path == "/users/login" {
@@ -17,10 +21,26 @@ func (m *LoginMiddlewareBuilder) CheckLogin() gin.HandlerFunc {
 			return
 		}
 		sess := sessions.Default(ctx)
-		if sess.Get("userId") == nil {
+		userId := sess.Get("userId")
+		if userId == nil {
 			// 中断，不要往后执行，也就是不要执行后面的业务逻辑
 			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
+		}
+		now := time.Now()
+		// 怎么知道，需要刷新了？
+		// 如果策略是每分钟刷新一次，怎么知道已经过了一分钟
+		const updateTimeKey = "update_time"
+		val := sess.Get(updateTimeKey)
+		lastUpdateTime, ok := val.(time.Time)
+		if val == nil || !ok || now.Sub(lastUpdateTime) > time.Minute {
+			// 这是第一次进来
+			sess.Set(updateTimeKey, now)
+			sess.Set("userId", userId)
+			err := sess.Save()
+			if err != nil {
+				fmt.Println(err)
+			}
 		}
 	}
 }
